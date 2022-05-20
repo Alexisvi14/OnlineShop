e83d9cd6f927eefbeef16f17b934d9f9b3d2113f
Alexisvi14 <alexisvillamayor96>
2022-05-19 21:00:36 -0300

    Made some changes on hierarchy

--- src/main/java/com/solvd/shop/Truck.java
+++
@@ -1,8 +0,0 @@
-package com.solvd.shop;
-
-import com.solvd.staff.Provider;
-
-public class Truck {
-    private long id;
-    private Provider provider;
-}
--- src/main/java/com/solvd/staff/Assistant.java
+++
@@ -1,8 +0,0 @@
-package com.solvd.staff;
-
-public class Assistant extends Employee {
-
-    public Assistant(long id, String firstName, String lastName) {
-        super(id, firstName, lastName);
-    }
-}

9f183bb527fb7ebe63d3c74408756a463c1aa22b
Alexisvi14 <alexisvillamayor96>
2022-06-01 21:29:48 -0300

    useless files deleted

--- src/main/java/com/solvd/DAO/ITruckDAO.java
+++
@@ -1,6 +0,0 @@
-package com.solvd.DAO;
-
-import com.solvd.shop.Truck;
-
-public interface ITruckDAO extends IBaseDAO <Truck> {
-}
--- src/main/java/com/solvd/DAO/service/JaxbService.java
+++
@@ -1,7 +0,0 @@
-package com.solvd.DAO.service;
-
-import com.solvd.bin.Employee;
-
-public interface JaxbService{
-    Object unmarshall(String xmlFilePath);
-}
